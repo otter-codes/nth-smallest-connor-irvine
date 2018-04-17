f69c7625d9c59d9f75acf6b8216ed8d3a4fe135c
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-04-17 13:05:09 +0100

    binned the empty list test as it was irrelevant add test 1 where list is reversed

--- src/main/scala/nthTerm.scala
+++ src/main/scala/nthTerm.scala
@@ -1,5 +1,9 @@
 object nthTerm extends App {
 
-  def
+  def assessor(OGList: List[Int], n: Int) : Int = {
+ val a = OGList.reverse
+
+    a.indexOf(n-1)
+  }
 
 }
--- src/test/scala/nthTermSpec.scala
+++ src/test/scala/nthTermSpec.scala
@@ -4,8 +4,8 @@ class nthTermSpec extends WordSpec with MustMatchers {
 
   "nthTerm" must {
 
-    "when given an empty list return 'no elements' " in {
-      nthTerm.assessor mustEqual "no elements"
+    "when given a list of '1,2,3,4,5' and n of '2' return '4' " in {
+      nthTerm.assessor(List(1,2,3,4,5), 2) mustEqual 4
     }
 
   }
