a709a49c9a32fe5b469eb73c679f707760408e95
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-04-19 14:55:06 +0100

    add FizzBuzz for all 3 response types

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,8 +1,8 @@
 object FizzBuzz {
 
-  def multiples(Digit: Int): String ={
- if (Digit % 3 == 0) return "Fizz"
-else "Buzz"
+  def multiples(Digit: Int): String = {
+   if (Digit%15==0) return "FizzBuzz"
+    else if (Digit%3==0) return "Fizz"
+   else (Digit%5==0); "Buzz"
   }
-
 }
